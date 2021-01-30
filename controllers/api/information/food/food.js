0da7bdc94040d40d33a8002a0bc2be5722ac0b53
linamkrtumyan <mkrtumyanlina@gmail.com>
2021-01-30 15:09:34 +0400

    Lina push 30 01

--- controllers/api/information/food/food.js
+++ controllers/api/information/food/food.js
@@ -1,77 +1,111 @@
 // const express = require('express');
 const router = express();
-const pool = require('../../../../database/db');
-const pgFunctions = require('../../../../pgFunctions');
-const writeInLogs = require('../../../../services/writeInLogsFile');
-const tokenVerify = require('../../../../middlewares/token/tokenVerify');
-
+const pool = require("../../../../database/db");
+const pgFunctions = require("../../../../pgFunctions");
+const writeInLogs = require("../../../../services/writeInLogsFile");
+const tokenVerify = require("../../../../middlewares/token/tokenVerify");
 
 router.use(express.json());
 
-router.get('/getFoods', tokenVerify, async (req, res) => {
-    try {
-        const allFoods = await pool.query(pgFunctions.food.usp_getFoods)
-            res.status(200).send({
-                allFoods: allFoods.rows
-            })
-        
-    }
-    catch(err)  {
-        writeInLogs(err);
-    }
-    
-})
+router.get("/getFoods", tokenVerify, async (req, res) => {
+  try {
+    const allFoods = await pool.query(pgFunctions.food.usp_getFoods);
+    res.status(200).send({
+      allFoods: allFoods.rows,
+    });
+  } catch (err) {
+    writeInLogs(err);
+  }
+});
 
-router.post('/addFood', tokenVerify, async (req, res) => {
-    try {
-        const { name, number, weight } = req.body;
-        const added = await pool.query(pgFunctions.food.usp_addFood, [name, number, weight])
-        res.status(200).send({ success: added.rows[0].success, errorMessage: added.rows[0].errorMessage, id: added.rows[0].id})
-    }
-    catch(err) {
-        writeInLogs(err);
-    }
-    
-    
-})
+router.post("/addFood", tokenVerify, async (req, res) => {
+  try {
+    const { name, number, weight } = req.body;
+    const added = await pool.query(pgFunctions.food.usp_addFood, [
+      name,
+      number,
+      weight,
+    ]);
+    res
+      .status(200)
+      .send({
+        success: added.rows[0].success,
+        errorMessage: added.rows[0].errorMessage,
+        id: added.rows[0].id,
+      });
+  } catch (err) {
+    writeInLogs(err);
+  }
+});
 
-router.post('/deleteFood', tokenVerify, async (req, res) => {
-    try {
-        const { id } = req.body;
-        const result = await pool.query(pgFunctions.food.usp_deleteFoods, [id])
-            res.status(200).send({ success: result.rows[0].success, errorMessage: result.rows[0].errormessage})
-        
-    }
-    catch(err) {
-        writeInLogs(err);
-    }
-    
-})
+router.post("/deleteFood", tokenVerify, async (req, res) => {
+  try {
+    const { id } = req.body;
+    const result = await pool.query(pgFunctions.food.usp_deleteFoods, [id]);
+    res
+      .status(200)
+      .send({
+        success: result.rows[0].success,
+        errorMessage: result.rows[0].errormessage,
+      });
+  } catch (err) {
+    writeInLogs(err);
+  }
+});
 
-
-router.post('/updateFood', tokenVerify, async (req, res) => {
-    try {
-        const { id, name, number, weight } = req.body;
-        const result = await pool.query(pgFunctions.food.usp_updateFood, [id, name, number, weight])
-        res.status(200).send({ success: result.rows[0].success, errorMessage: result.rows[0].errorMessage})
-       
-    }
-    catch(err)  {
-        writeInLogs(err);
-    }
-    
-})
-
+router.post("/updateFood", tokenVerify, async (req, res) => {
+  try {
+    const { id, name, number, weight } = req.body;
+    const result = await pool.query(pgFunctions.food.usp_updateFood, [
+      id,
+      name,
+      number,
+      weight,
+    ]);
+    res
+      .status(200)
+      .send({
+        success: result.rows[0].success,
+        errorMessage: result.rows[0].errorMessage,
+      });
+  } catch (err) {
+    writeInLogs(err);
+  }
+});
 
 router.get("/getCoefficient", async (req, res) => {
-    try {
-      const allCoef = await pool.query(pgFunctions.food.usp_getCoefficient);
-        res.status(200).send({
-          allCoef: allCoef.rows,
-        });
-    } catch (err) {
-      writeInLogs(err);
-    }
-  })
+  try {
+    const allCoef = await pool.query(pgFunctions.food.usp_getCoefficient);
+    res.status(200).send({
+      allCoef: allCoef.rows,
+    });
+  } catch (err) {
+    writeInLogs(err);
+  }
+});
 
-module.exports = router
+router.post("/updateWeight", tokenVerify, async (req, res) => {
+  console.log("object");
+  try {
+    // console.log(req.body);
+    console.log("object1");
+    const { id, weight, description, partnerid } = req.body;
+    // console.log(parseInt(weight));
+    const result = await pool.query(pgFunctions.food.usp_updateTheFood, [
+      id,
+      parseInt(weight),
+      description,
+      parseInt(partnerid),
+    ]);
+    console.log(result, "result");
+
+    res.status(200).send({
+      success: result.rows[0].success,
+      errorMessage: result.rows[0].errorMessage,
+    });
+  } catch (err) {
+    writeInLogs(err);
+  }
+});
+
+module.exports = router;
--- pgFunctions.js
+++ pgFunctions.js
@@ -1,55 +1,53 @@
-
 module.exports = {
-    auth: {
-        usp_login: 'SELECT * FROM usp_login($1)',
-    },
-    pool: {
-        usp_getPoolsAndDetails: 'SELECT * FROM "usp_getPoolsAndDetails"()',
-        usp_addPool: 'SELECT * FROM "usp_addPool"($1, $2)',
-        usp_deletePool: 'SELECT * FROM "usp_deletePool"($1)',
-        usp_updatePool: 'SELECT * FROM "usp_updatePool"($1, $2, $3)',
-        usp_getPools: 'SELECT * FROM "usp_getPools"()',
+  auth: {
+    usp_login: "SELECT * FROM usp_login($1)",
+  },
+  pool: {
+    usp_getPoolsAndDetails: 'SELECT * FROM "usp_getPoolsAndDetails"()',
+    usp_addPool: 'SELECT * FROM "usp_addPool"($1, $2)',
+    usp_deletePool: 'SELECT * FROM "usp_deletePool"($1)',
+    usp_updatePool: 'SELECT * FROM "usp_updatePool"($1, $2, $3)',
+    usp_getPools: 'SELECT * FROM "usp_getPools"()',
 
-        sale: {
-            usp_fishOut: 'SELECT * FROM "usp_fishOut"($1, $2, $3, $4, $5, $6)',
-        },
-        movement: {
-            usp_fishMove: 'SELECT * FROM "usp_fishMove"($1, $2, $3, $4, $5, $6)',
-        },
-        entrance: {
-            usp_fishIn: 'SELECT * FROM "usp_fishIn"($1, $2, $3, $4, $5, $6)',
-        }
-
+    sale: {
+      usp_fishOut: 'SELECT * FROM "usp_fishOut"($1, $2, $3, $4, $5, $6)',
     },
-    fish: {
-        usp_getFishes: 'SELECT * FROM "usp_getFishes"()',
-        usp_addFish: 'SELECT * FROM "usp_addFish"($1, $2)',
-        usp_deleteFish: 'SELECT * FROM "usp_deleteFish"($1)',
-        usp_updateFish: 'SELECT * FROM "usp_updateFish"($1, $2, $3)',
+    movement: {
+      usp_fishMove: 'SELECT * FROM "usp_fishMove"($1, $2, $3, $4, $5, $6)',
     },
-    food: {
-        usp_getFoods: 'SELECT * FROM "usp_getFoods"()',
-        usp_addFood: 'SELECT * FROM "usp_addFood"($1, $2, $3)',
-        usp_deleteFood: 'SELECT * FROM "usp_deleteFood"($1)',
-        usp_updateFood: 'SELECT * FROM "usp_updateFood"($1, $2, $3, $4)',
-        usp_getCoefficient: 'SELECT * FROM "usp_getCoefficient"()'
+    entrance: {
+      usp_fishIn: 'SELECT * FROM "usp_fishIn"($1, $2, $3, $4, $5, $6)',
     },
-    partner: {
-        usp_getPartners: 'SELECT * FROM "usp_getPartners"()',
-        usp_addPartner: 'SELECT * FROM "usp_addPartner"($1, $2, $3)',
-        usp_deletePartner: 'SELECT * FROM "usp_deletePartner"($1)',
-        usp_updatePartner: 'SELECT * FROM "usp_updatePartner"($1, $2, $3, $4)',
-    },
-    report: {
-        usp_createReport: 'SELECT * FROM "usp_createReport"($1)',
-        usp_getReportForMonth: 'SELECT * FROM "usp_getReportForMonth"($1, $2)',
-        usp_storeReport: 'SELECT * FROM "usp_storeReport"()',
-    },
-    feeding: {
-        usp_feed: 'SELECT * FROM "usp_feed"($1)',
-      },
-      losses: {
-        usp_loss: 'SELECT * FROM "usp_loss"($1)',
-      },
-
-}
+  },
+  fish: {
+    usp_getFishes: 'SELECT * FROM "usp_getFishes"()',
+    usp_addFish: 'SELECT * FROM "usp_addFish"($1, $2)',
+    usp_deleteFish: 'SELECT * FROM "usp_deleteFish"($1)',
+    usp_updateFish: 'SELECT * FROM "usp_updateFish"($1, $2, $3)',
+  },
+  food: {
+    usp_getFoods: 'SELECT * FROM "usp_getFoods"()',
+    usp_addFood: 'SELECT * FROM "usp_addFood"($1, $2, $3)',
+    usp_deleteFood: 'SELECT * FROM "usp_deleteFood"($1)',
+    usp_updateFood: 'SELECT * FROM "usp_updateFood"($1, $2, $3, $4)',
+    usp_getCoefficient: 'SELECT * FROM "usp_getCoefficient"()',
+    usp_updateTheFood: 'SELECT * FROM "usp_updateTheFood"($1, $2, $3, $4)',
+  },
+  partner: {
+    usp_getPartners: 'SELECT * FROM "usp_getPartners"()',
+    usp_addPartner: 'SELECT * FROM "usp_addPartner"($1, $2, $3)',
+    usp_deletePartner: 'SELECT * FROM "usp_deletePartner"($1)',
+    usp_updatePartner: 'SELECT * FROM "usp_updatePartner"($1, $2, $3, $4)',
+  },
+  report: {
+    usp_createReport: 'SELECT * FROM "usp_createReport"($1)',
+    usp_getReportForMonth: 'SELECT * FROM "usp_getReportForMonth"($1, $2)',
+    usp_storeReport: 'SELECT * FROM "usp_storeReport"()',
+  },
+  feeding: {
+    usp_feed: 'SELECT * FROM "usp_feed"($1)',
+  },
+  losses: {
+    usp_loss: 'SELECT * FROM "usp_loss"($1)',
+  },
+};
