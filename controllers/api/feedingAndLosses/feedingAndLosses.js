8ae4ab5f1a67b0ae916276d8c6bd58bae85cb3f1
varduhimehrabyan <varduhimehrabyan@mail.ru>
2021-06-10 17:07:37 +0400

    /

--- controllers/api/feedingAndLosses/feedingAndLosses.js
+++ controllers/api/feedingAndLosses/feedingAndLosses.js
@@ -10,7 +10,9 @@ router.use(express.json());
 router.post("/addFeed",tokenVerify, async (req, res) => {
   try {
     const { addFood } = req.body;
+    // console.log("addFood: ", addFood);
     const added = await pool.query(pgFunctions.feeding.usp_feed, [addFood]);
+    // console.log("added.rows: ", added.rows);
     res.status(200).send({
       success: added.rows[0].success,
       errorMessage: added.rows[0].errorMessage,
@@ -24,6 +26,7 @@ router.post("/addFeed",tokenVerify, async (req, res) => {
 router.post("/addLosse", tokenVerify, async (req, res) => {
     try {
       const { addLosses } = req.body;
+      console.log(addLosses);
       const added = await pool.query(pgFunctions.losses.usp_loss, [addLosses]);
       res.status(200).send({
         success: added.rows[0].success,
